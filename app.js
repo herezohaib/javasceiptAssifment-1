eedf78f94d311ecc80edef7a4659d8c8f98056e7
zohaib ali <herezohaib56@gmail.com>
2024-03-01 02:47:12 +0500

    javascript Assigmnet #1Chapter#1

---
+++ app.js
@@ -0,0 +1,13 @@
+  // ==== Chpter 1 (Q1) ===
+
+alert('Error! Please enter a valid password');
+ // ==== Chpter 1 (Q2) ===
+
+var fullname = "Zohiab\nAli";
+alert(fullname);
+ // ==== Chpter 1 (Q3) ===
+alert("Welcome to js land");
+ // ==== Chpter 1 (Q4) ===
+alert("Happy Coding!\nPrevent this page for creating additional dialogs");
+ // ==== Chpter 1 (Q5) ===
+alert("Hello,,, | can run JS through my web browser's console");
